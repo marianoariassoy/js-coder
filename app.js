865f943e0e82377bf8f379f7d4ff7941d54f5818
marianoariassoy <hola@marianoarias.soy>
2022-08-18 17:19:34 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let nombre = prompt("¡Hola! ¿Cómo te llamas?");
+alert("¡Hola " + nombre + "! mucho gusto. ");
+
+let anio = prompt("¿En que año naciste?");
+edad = 2022 - anio;
+console.log("Edad: " + edad);
+
+let signo = prompt("¿De que signo sos?");
+let color = prompt("¿Cual es tu color favorito?");
+alert(nombre + " tenes " + edad + " años o estar por cumplirlos, tu color favorito es " + color + " y sos del signo " + signo);
