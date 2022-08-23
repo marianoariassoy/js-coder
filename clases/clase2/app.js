299a4f73adfbde2bdff7ab521a1b93c841a6abc9
marianoariassoy <hola@marianoarias.soy>
2022-08-23 19:58:24 -0300

    clase2

---
+++ clases/clase2/app.js
@@ -0,0 +1,8 @@
+let numero1 = prompt("Ingresá un número");
+if (numero1 > 1000) alert("El número que ingresaste en mayor a 1000");
+
+let texto = prompt("Ingresá un texto");
+if (texto === "Hola") console.log("Ingresaste un texto igual a Hola");
+
+let numero2 = prompt("Ingresá un número");
+if (numero2 >= 10 && numero2 <= 50) alert("El número que ingresaste esta entre 10 y 50");
