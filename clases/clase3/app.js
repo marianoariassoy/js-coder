a06a5982aa3cee52818f593ab57685227a09e392
marianoariassoy <hola@marianoarias.soy>
2022-08-24 19:38:33 -0300

    clase 3 (desafio 1)

---
+++ clases/clase3/app.js
@@ -0,0 +1,28 @@
+// Consignas
+// 1. Pedir número mediante prompt y sumarle otro número en cada repetición,realizando una salida por cada resultado.
+
+let edad = parseInt(prompt("Ingresá tu edad por favor"));
+
+for (let index = 2026; index < 2060; index += 4) {
+  resultado = edad + (index - 2022);
+  alert("En el mundial de futbol de " + index + " tendrás " + resultado + " años de edad");
+}
+
+// 2. Pedir un texto mediante prompt, concatenar un valor en cada repetición, realizando una salida por cada resultado, hasta que se ingresa “ESC”.
+
+let textoConcatenado = "Te gustan estas péliculas o series: ";
+let texto;
+while (texto != "ESC") {
+  texto = prompt("Ingresá péliculas o series que te hayan gustado. Ingresa 'ESC' para finalizar. ");
+  textoConcatenado = textoConcatenado + texto + ", ";
+  if (texto != "ESC") alert(textoConcatenado);
+  else alert("Gracias!");
+}
+
+// 2.Pedir un número por prompt, repetir la salida del mensaje “Hola” la cantidad de veces ingresada.
+
+let veces = parseInt(prompt("Ingresá la cantidad de veces que deseas repetir la palabra 'Hola'"));
+
+for (let index = 1; index <= veces; index++) {
+  alert(index + ". Hola");
+}
